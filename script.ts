9e1dc1c6d58565783842e7efbbb5644270217735
Michael Beck <94383083+MMMJB@users.noreply.github.com>
2023-11-21 20:24:47 +0000

    literally zero progress

--- script.ts
+++ script.ts
@@ -1,5 +1,19 @@
 (() => {
   const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+  const ctx = canvas.getContext("2d")!;
+
+  let frame = 0;
+
+  const draw = (frame: number) => {
+    const { width, height } = canvas;
+
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width / 2, height / 2);
+
+    requestAnimationFrame(() => draw(frame + 1));
+  }
 
   const resize = () => {
     canvas.width = window.innerWidth;
@@ -8,4 +22,6 @@
 
   window.addEventListener("resize", resize);
   window.addEventListener("load", resize);
+
+  requestAnimationFrame(() => draw(frame));
 })();
